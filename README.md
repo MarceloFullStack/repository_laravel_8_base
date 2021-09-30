6f54e4755e7b368d6435d66a6fd711304f14b491
MarceloFullstack <fotofacil@live.com>
2021-09-30 13:15:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## os arquivos php a serem servidos devem estar dentro da pasta public
+com o docker e docker-compose instalado na sua maquina basta o seguinte comando: docker-compose up
+# esse setup contém o postgres na porta 5438
---
+++ public/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::get('/user', function (Request $request, UserRepository $repository) {
+//     $teste = $repository->getOne();
+//     return response()->json($teste);
+//     // return $request->user();
+// });
+Route::apiResource('users', UserController::class);
